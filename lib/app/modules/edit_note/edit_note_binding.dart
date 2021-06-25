81d26555598a05f0a61b78847cf86037dfe153b6
Jhon <jhon_presthon@hotmail.com>
2021-06-25 01:48:46 -0300

    icon

--- lib/app/modules/edit_note/edit_note_binding.dart
+++ lib/app/modules/edit_note/edit_note_binding.dart
@@ -1,6 +1,5 @@
 import 'package:get/get.dart';
 import 'package:notas/app/modules/edit_note/edit_note_controller.dart';
-import 'package:notas/app/modules/new_note/new_note_controller.dart';
 
 class EditNoteBinding extends Bindings {
   @override
